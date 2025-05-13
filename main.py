ce3d09eba6cf7550e16c161c807f034712f79e95
palpatinezw <emperorpalpatinezw@gmail.com>
2025-05-13 12:03:32 +0800

    Enable CORS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
 import datetime
 from dategen import Sikretekdate
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
